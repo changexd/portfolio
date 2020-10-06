8ed4a80a09ca6a0107d5673c0eb466be33f9d316
changexd <visa220033@gmail.ocm>
2020-10-06 14:19:22 +0800

    Updates

---
+++ precache-manifest.0c35e7b1f752bbb31b36a5019b1270c4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6aa918bf3063e5262761cd03b188db",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3c0e9bb14c0f53d5ff15",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4739f0b20bb7365327de",
+    "url": "/portfolio/static/css/main.4f0aaf01.chunk.css"
+  },
+  {
+    "revision": "3c0e9bb14c0f53d5ff15",
+    "url": "/portfolio/static/js/2.8a2a76f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.8a2a76f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4739f0b20bb7365327de",
+    "url": "/portfolio/static/js/main.f20baffd.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "38b4c6900117c3cae66d23ae67d68fb1",
+    "url": "/portfolio/static/media/background.38b4c690.jpg"
+  },
+  {
+    "revision": "8f5ce27564945d2c9a10ef827549a78c",
+    "url": "/portfolio/static/media/facebook.8f5ce275.png"
+  },
+  {
+    "revision": "785530eaff229a6bf83c1bdf55b2e177",
+    "url": "/portfolio/static/media/headshot.785530ea.png"
+  },
+  {
+    "revision": "24ec1755385e93c6bb4f62199c424aed",
+    "url": "/portfolio/static/media/resume.24ec1755.png"
+  }
+]);
