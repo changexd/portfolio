2b97f562e2bf74370cc303581a8e4b1266a7bec4
changexd <visa220033@gmail.ocm>
2020-12-16 16:16:24 +0800

    main css fixed with BEM and scss

--- src/Main.js
+++ src/Main.js
@@ -1,39 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import headshot from './headshot.png';
 
 const Main = () => {
   return (
-    <div className='mainfont mainbackground'>
+    <div className='main__container'>
       {' '}
-      <div
-        className='container text-center position-relative p-5'
-        style={{
-          top: '50%',
-          transform: 'translate(0,-50%)',
-          backgroundColor: 'rgba(0,0,0,0.3)',
-        }}
-      >
-        <div className='m-auto p-0 position-relative headshot'>
+      <div className='main__content__container'>
+        <div className='main__image'>
           <img width='100%' src={headshot}></img>
         </div>
-        <h1
-          className='blogpostfont'
-          style={{ color: 'white', fontSize: '60px' }}
-        >
-          Welcome!
-        </h1>
-        <h2
-          className='blogpostfont'
-          style={{ color: 'white', lineHeight: '2em' }}
-        >
-          {' '}
-          Here are some demonstrations of my projects
-        </h2>
-        <h2 className='blogpostfont' style={{ color: 'white' }}>
-          {' '}
-          Feel free to look around and click everything
-        </h2>
-        <h2 className='m-3 blogpostfont' style={{ color: 'white' }}>
+        <h1>Welcome!</h1>
+        <h2> Here are some demonstrations of my projects</h2>
+        <h2>Feel free to look around and click everything</h2>
+        <h2>
           You can click the navigation bar up here to check it out!
           <br />
           <br />
